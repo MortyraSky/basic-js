859c5ba882751115d7572a930075a50f3e9fa5f5
MortyraSky <gornii2014@gmail.com>
2020-03-20 22:04:14 +0300

    feat: added implement tower of hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,5 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let countTurns = Math.pow(2, disksNumber) - 1;
+    return { turns:countTurns, seconds:countTurns / (turnsSpeed / 3600) };
     // remove line with error and write your code here
 }
