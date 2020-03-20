16c7789f3d9fc654fbb1f5ca111ba62fbe6d83b7
MortyraSky <gornii2014@gmail.com>
2020-03-20 21:54:30 +0300

    feat: add implemet dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let arr = [];
+    let re = /,/g;
+    for (var i = 0; i < members.length; i++) {
+      if (typeof members[i] === 'string' && members[i].includes(' ')) {
+        arr.push(members[i].trim().substring(0, 1).toUpperCase());
+      } else if (typeof members[i] === 'string' && !members[i].includes(' ')) {
+        arr.push(members[i].substring(0, 1).toUpperCase());
+      }
+    }
+    return arr.sort().toString().replace(re, "");
+  }
+  return false;
+  }
